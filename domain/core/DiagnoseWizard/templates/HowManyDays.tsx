5479fb5102aa8cdce11da2b7f1d727b2b881080d
Rick Woltheus <rick.woltheus@lifely.nl>
2022-03-24 21:45:13 +0100

    *

--- domain/core/DiagnoseWizard/templates/HowManyDays.tsx
+++ domain/core/DiagnoseWizard/templates/HowManyDays.tsx
@@ -4,8 +4,7 @@ import { Select } from "../../Select/Select"
 export const HowManyDays: React.FunctionComponent<{}> = () => {
 return <Select id={'value'} name="value" 
     items={times(30, (num) => {
-
-        return { value: num, children: `${num} Days` }
+        return { value: num, label: `${num} Days` }
     })} 
     />
 }
--- domain/core/Select/Select.tsx
+++ domain/core/Select/Select.tsx
@@ -2,6 +2,7 @@
 
 import { DetailedHTMLProps, OptionHTMLAttributes, SelectHTMLAttributes } from "react"
 import styles from './Select.module.css'
+import ReactSelect from 'react-select';
 
 export interface SelectProps extends DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement> {
     items: DetailedHTMLProps<OptionHTMLAttributes<HTMLOptionElement>, HTMLOptionElement>[]
@@ -10,9 +11,8 @@ export interface SelectProps extends DetailedHTMLProps<SelectHTMLAttributes<HTML
 export const Select: React.FunctionComponent<SelectProps> = (props) => {
     const {items, ...restProps} = props
 
-   return <select {...restProps} className={styles.container}>
-       {props.items.map((item, i ) => 
-            <option key={i} {...item}>{item.children}</option>
-       )}
-   </select>
+   return <ReactSelect
+            options={items}
+        />
+   
 }
