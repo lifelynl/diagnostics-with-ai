551f78c391f33c924f70b09f98ce2fdc8bf91e68
Rick Woltheus <rick.woltheus@lifely.nl>
2022-03-24 21:28:04 +0100

    add how many days select

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div style={{paddingLeft: 8, paddingRight: 8}}><Component {...pageProps} /></div>
 }
 
 export default MyApp
