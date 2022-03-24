12ab2656b571ce089fab0c8217f09b41adaa9c91
Rick Woltheus <rick.woltheus@lifely.nl>
2022-03-24 22:12:59 +0100

    Fonts ar enice now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 
 import type { AppProps } from 'next/app'
+import './global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <div style={{paddingLeft: 8, paddingRight: 8}}><Component {...pageProps} /></div>
